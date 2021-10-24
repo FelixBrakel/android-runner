196c9010f3213a762b1bbef26335d32cb34b4aeb
Felix Brakel <felixbrakel@quicknet.nl>
2021-10-24 11:41:13 +0200

    Finish config

--- pre_testing.py
+++ pre_testing.py
@@ -3,12 +3,12 @@ import subprocess
 
 def main():
     configs = [
-        "android-runner/config/config_zoom_3.json",
-        "android-runner/config/config_zoom_5.json",
-        "android-runner/config/config_zoom_7.json",
-        "android-runner/config/config_zoom_9.json",
-        "android-runner/config/config_zoom_10.json",
-        "android-runner/config/config_meet_3.json",
+        # "android-runner/config/config_zoom_3.json",
+        # "android-runner/config/config_zoom_5.json",
+        # "android-runner/config/config_zoom_7.json",
+        # "android-runner/config/config_zoom_9.json",
+        # "android-runner/config/config_zoom_10.json",
+        # "android-runner/config/config_meet_3.json",
         "android-runner/config/config_meet_5.json",
         "android-runner/config/config_meet_7.json",
         "android-runner/config/config_meet_9.json",
