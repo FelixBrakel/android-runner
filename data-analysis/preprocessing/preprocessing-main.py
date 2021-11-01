16b08f1185d1dbe453e907f37c6f25f8771dcee5
leonhard <l.wattenbach@gmail.com>
2021-11-01 15:39:47 +0100

    Adapt preprocessing script to new data with bg

--- data-analysis/preprocessing/preprocessing-main.py
+++ data-analysis/preprocessing/preprocessing-main.py
@@ -9,15 +9,11 @@ def get_folder_names(path):
 
 
 # Configuration
-phone = "motog5"
+phone = "pixel3"
 raw_data_path = "/Volumes/Seagate HDD/VU/Green Lab/data-main"
 
 # The results will be pushed here before writing them into a csv file at the end
 result_rows = []
-
-# Define in which monkey runner scripts the camera/mic are active
-monkey_scripts_with_camera_on = ["1", "2"]
-monkey_scripts_with_mic_on = ["1", "4"]
 
 # For all folders in the raw data directory:
 experiment_setups = get_folder_names(raw_data_path)
@@ -32,11 +28,9 @@ for experiment_setup in experiment_setups:
         # Read config.json to obtain configuration
         config = json.load(open(os.path.join(raw_data_path, experiment_setup, experiment_run, "config.json"),))
         app_name = config["apps"][0]
-        duration = config["duration"]
 
-        # Determine which monkey runner script was used here
-        monkey_script_path = config["scripts"]["after_launch"][0]["path"]
-        monkey_script_number = re.search(".*[meet|zoom]([1-9])\.txt", monkey_script_path).group(1)
+        # Determine which setup for camera/microphone/background was used
+        setup_suffix = re.search(".*-([a-z]*)$", experiment_run).group(1)
         
         # Determine path and file names of energy log files
         log_files_path = os.path.join(raw_data_path, experiment_setup, experiment_run, "data", phone, app_name.replace(".", "-"), "batterystats")
@@ -56,21 +50,19 @@ for experiment_setup in experiment_setups:
                     "us.zoom.videomeetings": "Zoom"
                 }[app_name]
 
-                duration_in_minutes = duration / 60000
-
                 # Create a row for each measurement
                 result_rows.append({
                     "app": formatted_app_name,
                     "number_of_participants": number_of_participants,
-                    "camera": monkey_script_number in monkey_scripts_with_camera_on,
-                    "microphone": monkey_script_number in monkey_scripts_with_mic_on,
-                    "duration_in_minutes": duration_in_minutes,
+                    "camera": "c" in setup_suffix,
+                    "microphone": "m" in setup_suffix,
+                    "background": "b" in setup_suffix,
                     "joules": joule_value
                 })
 
 # Write results to a CSV file
 with open(os.path.join(raw_data_path, "..", "results-main.csv"), "w") as f:
-    writer = csv.DictWriter(f, fieldnames=["app", "number_of_participants", "camera", "microphone", "duration_in_minutes", "joules"])
+    writer = csv.DictWriter(f, fieldnames=["app", "number_of_participants", "camera", "microphone", "background", "joules"])
     writer.writeheader()
     writer.writerows(result_rows)
 
