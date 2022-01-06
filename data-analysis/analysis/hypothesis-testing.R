680a1cee25b219832b7a6c782129511e7fb9cd5a
leonhard <l.wattenbach@gmail.com>
2022-01-06 16:54:47 +0100

    Perform Benjamini-Hochberg correction

--- data-analysis/analysis/hypothesis-testing.R
+++ data-analysis/analysis/hypothesis-testing.R
@@ -13,7 +13,7 @@ zoom_data <- energy_data %>% filter(app == "Zoom")
 
 
 test <- function(data1, data2) {
-  wilcox.test(data1$joules, data2$joules)  
+  wilcox.test(data1$joules, data2$joules)
 }
 
 test(meet_data, zoom_data)
@@ -27,3 +27,7 @@ test(filter(zoom_data, number_of_participants == "2"), filter(zoom_data, number_
 test(filter(zoom_data, camera == "off"), filter(zoom_data, camera == "on"))
 test(filter(zoom_data, background == "off" & camera == "on"), filter(zoom_data, background == "on" & camera == "on"))
 test(filter(zoom_data, microphone == "off"), filter(zoom_data, microphone == "on"))
+
+# Perform Benjamini-Hochberg correction
+p = c(6.905e-11, 1.402e-11, 2.2e-16, 4.97e-05, 0.3577, 3.745e-06, 2.2e-16, 0.04005, 4.518e-06)
+p.adjust(p, method = "BH", n=length(p))
